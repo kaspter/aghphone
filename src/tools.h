2dacb81bf37b9e8c0cff192432ddc7f8e506d0d8
kramarczyk <kramarczyk@f13387f6-5b4d-0410-ba38-21fe5f7b40b5>
2008-05-20 20:30:20 +0000

    nomsg

--- src/tools.h
+++ src/tools.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2008  Mateusz Kramarczyk <kramarczyk (at) gmail (dot) com>
+ * Copyright (C) 2008  Tomasz Kijas <kijasek (at) gmail (dot) com>
+ * Copyright (C) 2008  Tomir Kryza <tkryza (at) gmail (dot) com>
+ * Copyright (C) 2008  Maciej Kluczny <kluczny (at) fr (dot) pl>
+ * Copyright (C) 2008  AGH University of Science and Technology <www.agh.edu.pl>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __TOOLS_H__INCLUDED__
 #define __TOOLS_H__INCLUDED__
 
@@ -12,8 +33,8 @@
 
 namespace agh {
 
-extern ost::IPV4Host getHostFromConnection(Ice::ConnectionPtr con);
-extern int chooseBestCodec(CodecsMap codecsMap);
+extern ost::IPV4Address getLocalAddressFromConnection(const Ice::ConnectionPtr& con);
+extern ost::IPV4Address getRemoteAddressFromConnection(const Ice::ConnectionPtr& con);
 
 } /* namespace agh */
 
