09272109c68328601c79bcc685fdf61a53d9ad98
kramarczyk <kramarczyk@f13387f6-5b4d-0410-ba38-21fe5f7b40b5>
2008-06-29 17:49:42 +0000

    added g711 codecs

--- trunk/src/mixer/module.mk
+++ trunk/src/mixer/module.mk
@@ -5,6 +5,10 @@ sources += $(local_src)
 
 programs += $(bin_dir)/mixer
 
-$(bin_dir)/mixer: $(local_objs) mixer/mixer.o common/globals.o common/tools.o common/codecfactory.o ice/iface.o ice/icecommon.o common/codecs/dummycodec.o 
+$(bin_dir)/mixer: $(local_objs) \
+	mixer/mixer.o common/globals.o common/tools.o common/codecfactory.o \
+	ice/iface.o ice/icecommon.o \
+	common/codecs/dummycodec.o \
+	common/codecs/g711.o common/codecs/legacy/g711/g711.o
 		$(ECHO_COMPILE_EXEC)
 		$(CXX) $(LDFLAGS) -o $@ $^
